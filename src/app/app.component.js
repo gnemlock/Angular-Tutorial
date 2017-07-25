1bbeef438f745b42cd7d2fc813cfe8d15d59eb53
Matthew <gnemlock@gmail.com>
2017-07-25 22:59:38 +1000

    3 - Multiple Components
    created a reusable component; made a component accept input; declared
    required application directives in the NgModule; bound a parent
    component to a child component.

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -18,12 +18,6 @@ var HEROES = [
     { id: 19, name: 'Magma' },
     { id: 20, name: 'Tornado' }
 ];
-var Hero = (function () {
-    function Hero() {
-    }
-    return Hero;
-}());
-exports.Hero = Hero;
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
@@ -38,7 +32,7 @@ AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
         styles: ["\n  \t\t.selected {\n  \t\t\tbackground-color: \t#CFD8DC !important;\n  \t\t\tcolor: \t\t\t\twhite;\n  \t\t}\n  \t\t.heroes {\n  \t\t\tmargin: \t\t\t0 0 2em 0;\n  \t\t\tlist-style-type: \tnone;\n  \t\t\tpadding: \t\t\t0;\n  \t\t\twidth: \t\t\t\t15em;\n  \t\t}\n  \t\t.heroes li {\n  \t\t\tcursor: \t\t\tpointer;\n  \t\t\tposition: \t\t\trelative;\n  \t\t\tleft: \t\t\t\t0;\n  \t\t\tbackground-color: \t#EEE;\n  \t\t\tmargin: \t\t\t.5em;\n  \t\t\tpadding: \t\t\t.3em 0;\n  \t\t\theight: \t\t\t1.6em;\n  \t\t\tborder-radius:\t\t4px;\n  \t\t}\n  \t\t.heries li.selected:hover {\n  \t\t\tbackground-color: \t#BBD8DC !important;\n  \t\t\tcolor: \t\t\t\twhite;\n  \t\t}\n  \t\t.heroes li:hover {\n  \t\t\tcolor: \t\t\t\t#607D8B;\n  \t\t\tbackground-color: \t#DDD;\n  \t\t\tleft: \t\t\t\t0.1em;\n  \t\t}\n  \t\t.heroes .text {\t\n  \t\t\tposition: \t\t\trelative;\n  \t\t\ttop: \t\t\t\t-3px;\n  \t\t}\n  \t\t.heroes .badge {\n  \t\t\tdisplay: \t\t\tinline-block;\n  \t\t\tfont-size: \t\t\tsmall;\n  \t\t\tcolor: \t\t\t\twhite;\n  \t\t\tpadding:\t\t\t0.8em 0.7em 0 0.7em;\n  \t\t\tbackground-color: \t#607D8B;\n  \t\t\tline-height: \t\t1em;\n  \t\t\tposition: \t\t\trelative;\n  \t\t\tleft: \t\t\t\t-1px;\n  \t\t\ttop: \t\t\t\t-4px;\n  \t\t\theight: \t\t\t1.8em;\n  \t\t\tmargin-right: \t\t0.8em;\n  \t\t\tborder-radius: \t\t4px 0 0 4px;\n  \t\t}\n  \t"],
-        template: "\n  \t\t<h1>{{title}}</h1>\n  \t\t<h2>My Heroes</h2>\n  \t\t<ul class=\"heroes\">\n  \t\t\t<li *ngFor=\"let hero of heroes\" \n  \t\t\t\t[class.selected]=\"hero === selectedHero\"\n  \t\t\t\t(click)=\"onSelect(hero)\">\n  \t\t\t\t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n  \t\t\t</li>\n  \t\t</ul>\n  \t\t<div *ngIf=\"selectedHero\">\n  \t\t\t<h2>{{selectedHero.name}} details!</h2>\n  \t\t\t<div><label>id: </label>{{selectedHero.id}}</div>\n  \t\t\t<div>\n  \t\t\t\t<label>name: </label>\n  \t\t\t\t<input [(ngModel)]=\"selectedHero.name\" placeholder=\"name\"/>\n  \t\t\t</div>\n  \t\t</div>\n  \t"
+        template: "    \n  \t\t<h1>{{title}}</h1>\n   \t \t<h2>My Heroes</h2>\n\t\t<ul class=\"heroes\">\n      \t\t<li *ngFor=\"let hero of heroes\"\n        \t\t[class.selected]=\"hero === selectedHero\"\n        \t\t(click)=\"onSelect(hero)\">\n        \t\t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n      \t\t</li>\n    \t</ul>\n    \t<hero-detail [hero]=\"selectedHero\"></hero-detail>\n  \t"
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component } 	from '@angular/core';
+
+import { Hero }			from './hero';
 
 const HEROES: Hero[] = [
 	{ id: 11, name: 'Mr. Nice' },
@@ -12,11 +14,6 @@ const HEROES: Hero[] = [
 	{ id: 19, name: 'Magma' },
 	{ id: 20, name: 'Tornado' }
 ];
-
-export class Hero {
-	id: number;
-	name: string;
-}
 
 @Component({
   	selector: 'my-app',
@@ -69,24 +66,17 @@ export class Hero {
   			border-radius: 		4px 0 0 4px;
   		}
   	`],
-  	template: `
+  	template: `    
   		<h1>{{title}}</h1>
-  		<h2>My Heroes</h2>
-  		<ul class="heroes">
-  			<li *ngFor="let hero of heroes" 
-  				[class.selected]="hero === selectedHero"
-  				(click)="onSelect(hero)">
-  				<span class="badge">{{hero.id}}</span> {{hero.name}}
-  			</li>
-  		</ul>
-  		<div *ngIf="selectedHero">
-  			<h2>{{selectedHero.name}} details!</h2>
-  			<div><label>id: </label>{{selectedHero.id}}</div>
-  			<div>
-  				<label>name: </label>
-  				<input [(ngModel)]="selectedHero.name" placeholder="name"/>
-  			</div>
-  		</div>
+   	 	<h2>My Heroes</h2>
+		<ul class="heroes">
+      		<li *ngFor="let hero of heroes"
+        		[class.selected]="hero === selectedHero"
+        		(click)="onSelect(hero)">
+        		<span class="badge">{{hero.id}}</span> {{hero.name}}
+      		</li>
+    	</ul>
+    	<hero-detail [hero]="selectedHero"></hero-detail>
   	`
 })
 
